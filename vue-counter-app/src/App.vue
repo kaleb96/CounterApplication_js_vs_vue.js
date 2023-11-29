d427fcdd67fb77e531e15988520af9a506ec19b6
kaleb96 <yes505304@gmail.com>
2023-11-29 16:26:42 +0900

    js vs vue
    Practice the difference between Using JS and Vue.js

---
+++ vue-counter-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="countBtn">-</button>
+      <button @click="countBtn">+</button>
+    </div>  
+  </main>
+</template>
+
+<script setup>
+  import {ref} from "vue";
+  const count = ref(0);
+
+</script>
+
+<style scoped>
+  main {
+    background-color: aliceblue;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  div {
+    text-align: center;
+  }
+</style>
