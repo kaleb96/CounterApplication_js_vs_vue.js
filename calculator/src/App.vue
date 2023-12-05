387e8ba33f2dad4940296ec84b1bc3e1cf492c24
kaleb96 <yes505304@gmail.com>
2023-12-05 18:22:32 +0900

    Update App.vue

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -7,26 +7,8 @@
       <div>
           
           <div class="buttonForm">
-            <!-- <div>Count Num</div>
-            <button>c</button>
-            <button>&#9003;</button> <br>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button> 
-            <button>&divide;</button> <br>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button> 
-            <button>x</button> <br>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button> 
-            <button>-</button> <br>
-            <button>.</button>
-            <button>0</button>
-            <button>=</button>
-            <button>+</button> -->
             <div>{{ addStr }}</div>
+            <div></div>
             <span v-for="(row, idx) in matrix" :key="idx"> <br>
             <button  @click="inputNum(row[cIdx])" v-for="(cell, cIdx) in row" :key="cIdx"> {{ row[cIdx] }} </button>
             </span>
@@ -42,7 +24,6 @@ import { ref } from 'vue'
 // const count = ref('');
 // const operators = ref('');
 const addStr = ref('');
-
 const matrix = ref([
   ['c', '<'],
   ['7', '8', '9', '/'],
@@ -54,20 +35,19 @@ const matrix = ref([
 const inputNum = (param) => {
   // console.log('inputNum = ' + num );
   let value = ''
-  if(isNaN(param)) { //파라미터가 정수인 경우
-    
-    value = param;
-    // console.log('count.value = ' + count.value);
+  
 
-  } else { // 기호인 경우
-    // operators.value = param;
-    value = param;
-    // console.log('operators.value = ' + operators.value);
-
-  }
+    if(!(isNaN(param))) { // 정수인 경우
+      value = param;
+    }
+  
+    if(isNaN(param)) { // 연산자인 경우
+      value = param;
+    }
+  
   addStr.value += value; //문자열 합치기
 
-
+  // return addStr하거나 다른 콜백 함수를 통해서 다시 받기
 }
 
 </script>
