ff597d54d0f2d7a8399d302afbb82f7930fb6ded
kaleb96 <yes505304@gmail.com>
2023-12-06 16:26:55 +0900

    implement Basic calculator functions
    - implement Basic calculator functions(-, + ,/ ,%, =)

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -7,7 +7,11 @@
       <div>
           
           <div class="buttonForm">
-            <div>{{ addStr }}</div>
+            <div>첫번째 숫자{{ firstNum }}</div>
+            <div>두 번째 연산자 {{ operatorStr }}</div>
+            <div>세 번째 숫자 {{ secondNum }}</div>
+            <div>네 번째 결과 지 {{ firstNum }} {{ operatorStr }} {{ secondNum }} = </div>
+            <div>결과 flag {{ flag }}</div>
             <div></div>
             <span v-for="(row, idx) in matrix" :key="idx"> <br>
             <button  @click="inputNum(row[cIdx])" v-for="(cell, cIdx) in row" :key="cIdx"> {{ row[cIdx] }} </button>
@@ -21,9 +25,12 @@
 <script setup>
 import { ref } from 'vue'
 
-// const count = ref('');
-// const operators = ref('');
-const addStr = ref('');
+
+const firstNum = ref(0);
+const operatorStr = ref('');
+const secondNum = ref(0);
+const flag = ref('');
+
 const matrix = ref([
   ['c', '<'],
   ['7', '8', '9', '/'],
@@ -32,22 +39,94 @@ const matrix = ref([
   ['.', '0','=','+']
 ])
 
-const inputNum = (param) => {
-  // console.log('inputNum = ' + num );
-  let value = ''
-  
 
-    if(!(isNaN(param))) { // 정수인 경우
-      value = param;
+const inputNum = (param) => { //버튼 input값에 따른 연산 판단
+  
+  let regex = /[0-9.]/g;
+
+ 
+  if(regex.test(param)) { // 정수 또는 '.' 인 경우
+    
+    if(operatorStr.value == '') { // firstNum
+      
+      if(firstNum.value == 0) {
+        firstNum.value = param;
+      } else {
+        firstNum.value += param;
+      }
+
+    } else {  // secondeNum
+      
+      if(secondNum.value == 0) {
+        secondNum.value = param;
+      } else {
+        secondNum.value += param;
+      }
     }
   
-    if(isNaN(param)) { // 연산자인 경우
-      value = param;
-    }
-  
-  addStr.value += value; //문자열 합치기
+  } else { // 연산자인 경우
+    
+    if((param == '=')) { // = 인경우, firstNum, operatorStr, secondNum이 있는 경우 -> 결과 연산!
+      
+      if(operatorStr != '') {
 
-  // return addStr하거나 다른 콜백 함수를 통해서 다시 받기
+        computeResult(operatorStr);
+      }
+
+    } else { // 연산자인 경우
+
+      operatorStr.value = param;
+    }
+  }
+
+}
+
+const computeResult = (param) => { // 연산자 구분, 계산
+  
+  let co = param.value; 
+  let value1 = parseFloat(firstNum.value);
+  let value2 = parseFloat(secondNum.value);
+  let result = '';
+  switch (co) {
+
+    case '-':
+      result = value1 - value2;
+      formatData(result);
+      break;
+    
+    case '+':
+      result = value1 + value2;
+      formatData(result);
+      break;
+    
+    case '/':
+      result = value1 / value2;
+      formatData(result);
+      break;
+    
+    case '%':
+      result = value1 % value2;
+      formatData(result);
+      break;
+    
+    case 'x':
+    result = value1 * value2;
+    formatData(result);
+    break;  
+
+    default:
+      console.log('정의 되지 않은 계산입니다.');
+      formatData(result);
+      break;
+  }
+}
+
+const formatData= (param) => { // 데이터 초기화
+    
+    firstNum.value = param;
+    secondNum.value = 0;
+    operatorStr.value = '';
+    flag.value = param;
 }
 
 </script>
