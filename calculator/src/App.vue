91cd2782330f2bc6acd73bce4035ea8abf30a603
kaleb96 <yes505304@gmail.com>
2023-12-06 18:02:05 +0900

    #4
    Add
    -  implement Clear all function
    -  implement Clear entry function

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -7,11 +7,12 @@
       <div>
           
           <div class="buttonForm">
-            <div>첫번째 숫자{{ firstNum }}</div>
-            <div>두 번째 연산자 {{ operatorStr }}</div>
-            <div>세 번째 숫자 {{ secondNum }}</div>
-            <div>네 번째 결과 지 {{ firstNum }} {{ operatorStr }} {{ secondNum }} = </div>
-            <div>결과 flag {{ flag }}</div>
+            <div>
+              <span v-if="!secondNum">{{ firstNum }}</span>
+              <span v-if="!secondNum">{{ operatorStr }}</span>
+              <span v-if="secondNum">{{ firstNum }} {{ operatorStr }} {{ secondNum }} = </span>
+            </div>
+            <div>결과 {{ flag }}</div>
             <div></div>
             <span v-for="(row, idx) in matrix" :key="idx"> <br>
             <button  @click="inputNum(row[cIdx])" v-for="(cell, cIdx) in row" :key="cIdx"> {{ row[cIdx] }} </button>
@@ -69,14 +70,38 @@ const inputNum = (param) => { //버튼 input값에 따른 연산 판단
     
     if((param == '=')) { // = 인경우, firstNum, operatorStr, secondNum이 있는 경우 -> 결과 연산!
       
-      if(operatorStr != '') {
+      if(param != '') {
 
         computeResult(operatorStr);
       }
 
     } else { // 연산자인 경우
 
-      operatorStr.value = param;
+      if(param == 'c') { // c버튼 Clear All
+        
+        firstNum.value = 0;
+        secondNum.value = 0;
+        operatorStr.value = '';
+        flag.value = '';
+
+      } else if(param == '<') { // < 버튼 Clear Entry
+
+        if(flag.value == '') {
+          let value1 = firstNum.value;
+          let value2 = secondNum.value;
+          let value3 = operatorStr.value;
+          clearEntry(value1, value2, value3);
+        
+        } else {
+          firstNum.value = 0;
+          secondNum.value = 0;
+          operatorStr.value = '';
+        }
+        
+      }else {
+        
+        operatorStr.value = param;
+      }
     }
   }
 
@@ -130,6 +155,28 @@ const formatData= (param) => { // 데이터 초기화
     flag.value = param;
 }
 
+const clearEntry = (value1, value2, value3) => { // Clear Entry 취소 
+  let nv = '';
+
+  if(value2) { // secondNum에 clear Entry 작동
+      
+      nv = [...value2].splice(0, value2.length-1);  
+      secondNum.value = nv.join('');
+  
+  } else if(value1 && !value2 && !value3) { //fisrtNum에  Clear Entry 작동
+
+      nv = [...value1].splice(0, value1.length-1);
+      firstNum.value = nv.join('');
+  
+  } else if(value1 && value3) { 
+    // firatNum, Operators있는 경우 Clear Entry 작동 X
+ 
+  } else {
+    console.log('check');
+  }
+
+  
+}
 </script>
 
 <style>
